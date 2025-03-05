7158f55271c081117f721f445a12e31a54caa698
Sanjaya Niroula <niroulasanjaya42@fmail.com>
2025-03-06 00:14:30 +0545

    decided components

--- BDT/src/App.jsx
+++ BDT/src/App.jsx
@@ -1,15 +1,18 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
+import Navbar from './components/Navbar'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-     <div><h1>BLOOD</h1></div>
+    <Navbar/>
+    <h1 class='text-5xl font-bold underline' >BloodFind</h1>
     </>
   )
 }
 
 export default App
+ 
---
+++ BDT/src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div class='w-full bg-[#D32F2F] flex justify-between p-4' >
+        <div>
+            <div class="bg-white-900"> <h2 class='text-white'>BloodLine</h2></div>
+            
+        </div>
+        <div>
+        <div class="bg-white-900"> <h2 class='text-white'>RightSideDemo</h2></div>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar;
